055fe821019f99bdf260e611623ca7af6a427771
david@DAVID-PC <3200210+davidjenni@users.noreply.github.com>
2023-09-04 22:46:33 -0700

    implement memory and address bus
    also adding ci PS script for Windows

--- ci.sh
+++ ci.sh
@@ -4,10 +4,12 @@ export RUSTFLAGS="-Dwarnings"
 
 echo "== Build:"
 cargo clean
-cargo build
+cargo build --all-targets --all-features
 echo "== Test:"
 cargo test --all-targets --all-features
 echo "== Lint:"
 cargo clippy --all-targets --all-features
 echo "== Format:"
 cargo fmt --all -- --check
+
+echo "Completed!"
--- emulator/src/lib.rs
+++ emulator/src/lib.rs
@@ -1,1 +1,4 @@
+pub mod address_bus;
+pub mod cpu;
+pub mod memory;
 pub mod status_register;
