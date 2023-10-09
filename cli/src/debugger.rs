b55b29f8cb1d5ec6054c35ad72713e57f531b52b
david@DAVID-PC <3200210+davidjenni@users.noreply.github.com>
2023-10-08 21:22:21 -0700

    use parsed addr ranges for disassemble and memory verbs

--- cli/src/debugger.rs
+++ cli/src/debugger.rs
@@ -1,6 +1,7 @@
+use std::cmp;
 use std::io;
 
-use crate::dbg_cmd_parser::{parse_cmd, DebugCmdError, DebugCommand};
+use crate::dbg_cmd_parser::{parse_cmd, AddressRange, DebugCmdError, DebugCommand};
 use mos6502_emulator::{Cpu, CpuError, CpuRegisterSnapshot};
 
 pub struct Debugger<R, W, E> {
@@ -9,7 +10,8 @@ pub struct Debugger<R, W, E> {
     #[allow(dead_code)]
     stderr: E,
     last_cmd: DebugCommand,
-    last_addr: Option<u16>,
+    last_prog_addr: Option<u16>,
+    last_mem_addr: Option<u16>,
 }
 
 impl<R, W, E> Debugger<R, W, E>
@@ -24,7 +26,8 @@ where
             stdout: Box::new(io::stdout()),
             stderr: Box::new(io::stderr()),
             last_cmd: DebugCommand::Invalid,
-            last_addr: None,
+            last_prog_addr: None,
+            last_mem_addr: None,
         }
     }
 
@@ -39,39 +42,42 @@ where
                 DebugCommand::Step => {
                     let snapshot = cpu.step()?;
                     self.print_snapshot(cpu, snapshot)?;
-                    self.last_addr = None;
+                    self.last_prog_addr = None;
                 }
                 DebugCommand::Continue => {
                     let snapshot = cpu.run(Some(cpu.get_pc()))?;
                     self.print_snapshot(cpu, snapshot)?;
-                    self.last_addr = None;
+                    self.last_prog_addr = None;
                 }
-                DebugCommand::Disassemble(_) => {
-                    // TODO: consume range
-                    let addr = match self.last_addr {
-                        Some(addr) => addr,
-                        None => cpu.get_pc(),
-                    };
-                    let (lines, next_addr) = cpu.disassemble(addr, 10)?;
-                    for line in lines {
-                        self.writeln(format!("  {}", line).as_str());
+                DebugCommand::Disassemble(addr_range) => {
+                    let (start, end, line_cnt) =
+                        calculate_range(self.last_prog_addr, addr_range, cpu);
+                    let mut next_addr = start;
+                    loop {
+                        let (lines, next) = cpu.disassemble(next_addr, line_cnt.unwrap_or(10))?;
+                        for line in lines {
+                            self.writeln(format!("  {}", line).as_str());
+                        }
+                        next_addr = next;
+                        if next_addr >= end {
+                            break;
+                        }
                     }
-                    self.last_addr = Some(next_addr);
+                    self.last_prog_addr = Some(next_addr);
                 }
-                DebugCommand::Memory(_) => {
-                    // TODO: consume range
-                    let addr = match self.last_addr {
-                        Some(addr) => addr,
-                        None => cpu.get_pc(),
-                    };
-                    let mut msg = format!("  {:04X}:", addr);
-                    for i in 0..16 {
-                        msg.push_str(
-                            format!(" {:02X}", cpu.get_byte_at(addr.wrapping_add(i + 1))?).as_str(),
-                        );
+                DebugCommand::Memory(addr_range) => {
+                    let (start, end, _) = calculate_range(self.last_mem_addr, addr_range, cpu);
+                    let mut next_addr = start;
+                    while let Ok((line, next)) =
+                        self.write_memory_ln(cpu, next_addr, cmp::min(end - next_addr, 16))
+                    {
+                        self.writeln(line.as_str());
+                        next_addr = next;
+                        if next_addr >= end {
+                            break;
+                        }
                     }
-                    self.writeln(msg.as_str());
-                    self.last_addr = Some(addr + 16);
+                    self.last_mem_addr = Some(next_addr);
                 }
                 DebugCommand::Invalid => {
                     self.show_usage();
@@ -84,6 +90,21 @@ where
             }
         }
         Ok(cpu.get_register_snapshot())
+    }
+
+    fn write_memory_ln(
+        &self,
+        cpu: &mut Box<dyn Cpu>,
+        addr: u16,
+        cnt: u16,
+    ) -> Result<(String, u16), DebugCmdError> {
+        let mut msg = format!("  {:04X}:", addr);
+        let mut next_addr = addr;
+        for i in 0..cnt {
+            msg.push_str(format!(" {:02X}", cpu.get_byte_at(addr + i)?).as_str());
+            next_addr = next_addr.wrapping_add(1);
+        }
+        Ok((msg, next_addr))
     }
 
     fn writeln(&mut self, msg: &str) {
@@ -120,7 +141,11 @@ where
             },
         };
         if cmd == DebugCommand::Repeat {
-            cmd = self.last_cmd
+            cmd = match self.last_cmd {
+                DebugCommand::Memory(_) => DebugCommand::Memory(AddressRange::Default),
+                DebugCommand::Disassemble(_) => DebugCommand::Disassemble(AddressRange::Default),
+                _ => self.last_cmd,
+            }
         } else {
             self.last_cmd = cmd;
         }
@@ -175,6 +200,25 @@ pub fn print_register(writer: &mut dyn io::Write, snapshot: CpuRegisterSnapshot)
     writer.write_all(msg.as_bytes()).unwrap();
 }
 
+fn calculate_range(
+    last_addr: Option<u16>,
+    addr_range: AddressRange,
+    cpu: &mut Box<dyn Cpu>,
+) -> (u16, u16, Option<usize>) {
+    let pc = cpu.get_pc();
+    let (start, end, line_cnt) = match addr_range {
+        AddressRange::StartEnd((start, end)) => (start, end, None),
+        AddressRange::StartLines((start, line_cnt)) => {
+            (start, start.wrapping_add(line_cnt as u16), Some(line_cnt))
+        }
+        AddressRange::Default => match last_addr {
+            Some(addr) => (addr, addr.wrapping_add(16), None),
+            None => (pc, pc.wrapping_add(16), None),
+        },
+    };
+    (start, end, line_cnt)
+}
+
 #[cfg(test)]
 mod tests {
     // use anyhow::Ok;
@@ -208,14 +252,15 @@ mod tests {
     }
 
     #[test]
-    fn debug_loop() -> Result<(), DebugCmdError> {
-        let mut spy = Spy::new("disassemble\nstep\n\nquit\n");
+    fn debug_loop_disassemble() -> Result<(), DebugCmdError> {
+        let mut spy = Spy::new("disassemble\n\nstep\n\nquit\n");
         let mut debugger = Debugger {
             stdin: Box::new(spy.stdin),
             stdout: &mut spy.stdout,
             stderr: &mut spy.stderr,
             last_cmd: DebugCommand::Invalid,
-            last_addr: None,
+            last_prog_addr: None,
+            last_mem_addr: None,
         };
         let mut cpu = mos6502_emulator::create_cpu(mos6502_emulator::CpuType::MOS6502)?;
         cpu.load_program(0x0300, &[0xA9, 0x42, 0x85, 0x0F, 0x00], true)?;
@@ -233,6 +278,54 @@ mod tests {
     }
 
     #[test]
+    fn debug_loop_disassemble_by_lines() -> Result<(), DebugCmdError> {
+        let mut spy = Spy::new("di $0300,4\n\nquit\n");
+        let mut debugger = Debugger {
+            stdin: Box::new(spy.stdin),
+            stdout: &mut spy.stdout,
+            stderr: &mut spy.stderr,
+            last_cmd: DebugCommand::Invalid,
+            last_prog_addr: None,
+            last_mem_addr: None,
+        };
+        let mut cpu = mos6502_emulator::create_cpu(mos6502_emulator::CpuType::MOS6502)?;
+        cpu.load_program(0x0300, &[0xA9, 0x42, 0x85, 0x0F, 0x00], true)?;
+        cpu.set_pc(0x0300)?;
+        debugger.debug_loop(&mut cpu)?;
+
+        let stdout = spy.get_stdout();
+        // println!("{}", stdout);
+        assert!(stdout.contains("PC: 0300: A: 00 X: 00 Y: 00 S: 00000010 SP: 01FF"));
+        assert!(stdout.contains("0300 LDA #$42"));
+        Ok(())
+    }
+
+    #[test]
+    fn debug_loop_memory() -> Result<(), DebugCmdError> {
+        let mut spy = Spy::new("memory 0x020..0x42\n\nquit\n");
+        let mut debugger = Debugger {
+            stdin: Box::new(spy.stdin),
+            stdout: &mut spy.stdout,
+            stderr: &mut spy.stderr,
+            last_cmd: DebugCommand::Invalid,
+            last_prog_addr: None,
+            last_mem_addr: None,
+        };
+        let mut cpu = mos6502_emulator::create_cpu(mos6502_emulator::CpuType::MOS6502)?;
+        cpu.set_pc(0x0300)?;
+        #[allow(unused_variables)]
+        debugger.debug_loop(&mut cpu)?;
+
+        let stdout = spy.get_stdout();
+        // println!("{}", stdout);
+        assert!(stdout.contains("0020: 00 00 00 00"));
+        assert!(stdout.contains("0030: 00 00 00 00"));
+        assert!(stdout.contains("0040: 00"));
+        assert!(stdout.contains("0041: 00 00 00 00"));
+        Ok(())
+    }
+
+    #[test]
     fn usage() -> Result<(), DebugCmdError> {
         let mut spy = Spy::new("help\nquit\n");
         let mut debugger = Debugger {
@@ -240,7 +333,8 @@ mod tests {
             stdout: &mut spy.stdout,
             stderr: &mut spy.stderr,
             last_cmd: DebugCommand::Invalid,
-            last_addr: None,
+            last_prog_addr: None,
+            last_mem_addr: None,
         };
         let mut cpu = mos6502_emulator::create_cpu(mos6502_emulator::CpuType::MOS6502)?;
         cpu.load_program(0x0300, &[0x00], true)?;
